5c0f2011f66fa82962f911788cbafff783c032a9
Michal Hanebach <michal.hanebach@gmail.com>
2018-02-19 11:10:52 +0100

    BookDao: ad initializer and finalizer methods for sql connection

--- src/book/BookDao.java
+++ src/book/BookDao.java
@@ -2,8 +2,44 @@ package book;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.sql.*;
+
+import exceptionlog.ExceptionLog;
 
 public class BookDao {
+
+  private Connection con;
+  private static String JDBC = "jdbc:sqlite:database/database.db";
+
+  public void initializeConnection() {
+
+    try {
+
+        Class.forName("org.sqlite.JDBC");
+    } catch(ClassNotFoundException e) {
+        System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+        return;
+    }
+    try {
+
+      con = DriverManager.getConnection(BookDao.JDBC);
+    } catch(SQLException e) {
+      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+    }
+  }
+
+  public void finalizeConnection() {
+
+    if (con != null) {
+
+      try {
+
+        con.close();
+      } catch(SQLException e) {
+        System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+      }
+    }
+  }
 
   public boolean addBook(Book book) {
 
